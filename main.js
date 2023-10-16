44a702acf706c0d63b5fe7fe3b8a8aa518f15140
CJH <jaehyuck009603@gmail.com>
2023-10-16 13:00:09 +0900

    자바스크립트 연결 변수let, var , const 호이스팅

---
+++ main.js
@@ -0,0 +1,22 @@
+'use strict'
+// let num = 10+20;
+// let nums = 50;
+// nums = '40';
+// console.log(num);
+// console.log(nums);
+
+//호이스팅
+// var num;
+// console.log(num);
+// num = 10;
+
+// let a = 5;
+// {
+//     let a =3
+// }
+// console.log(a);
+//지역변수 let var차이
+
+// const num =10; //변하지 않는수
+// num = 30;
+// console.log(num);
