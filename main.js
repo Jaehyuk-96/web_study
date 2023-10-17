1cfb94fe3d2cf8d95554607e2e32e8fe604b2fd4
CJH <jaehyuck009603@gmail.com>
2023-10-17 17:14:45 +0900

    호이스팅, 문자열, 작은따옴표, 백틱활용, 탬플릿 문자열 for of문 ,==, ===차이 알아보기

--- main.js
+++ main.js
@@ -20,3 +20,51 @@
 // const num =10; //변하지 않는수
 // num = 30;
 // console.log(num);
+
+
+// console.log(text); // (선언 + 초기화 된 상태)
+// text = 'Hanamon!'; // (선언 + 초기화 + 할당 된 상태)
+// var text;//호이스팅
+// console.log(text);
+
+// foo1(); // 함수 선언문에서는 호이스팅 일어난다.
+// foo2(); // 함수 표현식이라서 호이스팅 안된다.
+// function foo1() {
+//   console.log('Hello');
+// }
+// var foo2 = function() {
+//   console.log('world');
+// }
+
+// let string =17 + 17;
+// console.log(string);
+
+// let string = `문자열(")
+// 작은따옴표(')
+// 줄바꿈`;
+// console.log(string); 백틱활용
+
+// let num1 = 10;
+// let num2 = 0.1;
+// let string = `${num1}과 ${num2}을 곱하면 ${num1*num2}이다.`탬플릿 문자열
+// console.log(string);
+
+// let array = ['abc' , 10 , true, undefined , null, [] , {}, function(){}];
+// for(var value of array){
+//     console.log(value);
+// }for of 문
+
+// let studentScore = {
+//     koreanScore:80,
+//     englishScore:70,
+//     mathScore:90,
+//     scienceScore:60
+// };
+// console.log(studentScore.koreanScore)
+// console.log(studentScore['englishScore']);
+
+// let a=5;
+// let b='5';
+// console.log(a===b); 자료형
+// console.log(a==b); 값
+
